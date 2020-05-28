9eba53988cf194dfd4a9051ca6141861574f42fc
maetdol <rndka9@gmail.com>
2020-05-28 17:56:57 +0900

    Refector #12 - Delete not using code Delete not used codes.

--- js/frame.js
+++ js/frame.js
@@ -18,20 +18,3 @@ function init() {
   }
 }
 init();
-
-function createElement({tag, textContent, htmlContent, classes, id}) {
-  let elem = document.createElement(tag);
-  if( htmlContent ) {
-    elem.innerHTML = htmlContent;
-  }
-  if( textContent ) {
-    textContent.split('\n').map( t => elem.append( new Text(t) ));
-  }
-  if( id ) {
-    elem.setAttribute('id', id);
-  }
-  if( classes ) {
-    elem.className = classes.join(' ');
-  }
-  return elem;
-}
