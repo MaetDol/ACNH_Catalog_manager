d14694839dc3fb4440a6c7e793e53e903511a75e
maetdol <rndka9@gmail.com>
2020-05-29 17:52:07 +0900

    Style #14 - Refector database and columns naming
    Whole tables and columns changed.

--- create_tables.sql
+++ create_tables.sql
@@ -1,66 +1,74 @@
-CREATE TABLE sheets (
-    sh_password varchar(300) NOT NULL,
-    sh_title varchar(255) NOT NULL,
-    sh_need_update boolean DEFAULT true,
-    PRIMARY KEY (sh_password)
-);
+CREATE DATABASE acnh_catalog_manager;
+USE acnh_catalog_manager;
 
-CREATE TABLE sheet_rows (
-    rw_id int NOT NULL AUTO_INCREMENT,
-    rw_complete boolean DEFAULT false,
-    rw_deleted boolean DEFAULT false,
-    sh_password varchar(300) NOT NULL,
-    fr_id int NOT NULL,
-    PRIMARY KEY(rw_id),
-    CONSTRAINT sh_password FOREIGN KEY (sh_password) REFERENCES sheets (sh_password),
-    CONSTRAINT fr_id FOREIGN KEY (fr_id) REFERENCES furnitures (fr_id)
-);
-
-CREATE TABLE row_variation (
-    rv_owned boolean DEFAULT false,
-    rw_id int NOT NULL,
-    vr_id int NOT NULL,
-    KEY rw_id (rw_id),
-    KEY vr_id (vr_id)
-);
-
-CREATE TABLE variations (
-    vr_id int NOT NULL AUTO_INCREMENT,
-    vr_en varchar(20) NOT NULL,
-    vr_kr varchar(30),
-    vr_img varchar(150),
-    fr_id int NOT NULL,
-    PRIMARY KEY(vr_id)
-);
-ALTER TABLE variations ADD CONSTRAINT FOREIGN KEY (fr_id) REFERENCES furnitures (fr_id);
-
-CREATE TABLE furnitures (
-    fr_id int NOT NULL AUTO_INCREMENT,
-    fr_en varchar(50) NOT NULL,
-    fr_kr varchar(80), 
-    fr_price int,
-    fr_preview_vr_id int,
-    PRIMARY KEY(fr_id),
-    CONSTRAINT fr_preview_vr_id FOREIGN KEY(fr_preview_vr_id) REFERENCES variations (vr_id)
-);
-
-CREATE TABLE tags (
-    tg_id int NOT NULL AUTO_INCREMENT,
-    tg_category varchar(40) NOT NULL,
-    tg_content varchar(80) NOT NULL,
-    PRIMARY KEY(tg_id)
-);
-
-CREATE TABLE using_tags (
-    tg_id int NOT NULL,
-    fr_id int NOT NULL,
-    CONSTRAINT tg_id FOREIGN KEY(tg_id) REFERENCES tags (tg_id),
-    CONSTRAINT fr_id FOREIGN KEY(fr_id) REFERENCES furnitures (fr_id)
+CREATE TABLE `users` (
+    `id` varchar(30) NOT NULL,
+    `password` varchar(30) NOT NULL,
+    `sign_up_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY(`id`)
 );
 
 
+CREATE TABLE `sheets` (
+    `id` int NOT NULL AUTO_INCREMENT,
+    `user_id` varchar(30) NOT NULL,
+    `title` varchar(255) NOT NULL,
+    `view` int DEFAULT 0,
+    `is_need_update` boolean DEFAULT true,
+    PRIMARY KEY(`id`),
+    CONSTRAINT FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+);
 
--- dummy datas
+CREATE TABLE `variants` (
+    `id` int NOT NULL AUTO_INCREMENT,
+    `variant_id` VARCHAR(5) NOT NULL DEFAULT '0_0',
+    `color_en` varchar(20) NOT NULL,
+    `color_kr` varchar(30) NOT NULL,
+    `image_url` varchar(10),
+    `item_id` int NOT NULL,
+    PRIMARY KEY(`id`)
+);
 
--- Insert furniture information.
-INSERT furnitures (fr_en, fr_kr, price) VALUES('English name!', 'Anyway KR name!', 37000);
+CREATE TABLE `items` (
+    `id` int NOT NULL,
+    `name_en` varchar(50) NOT NULL,
+    `name_kr` varchar(80) NOT NULL, 
+    `preview_variant_id` int,
+    UNIQUE KEY(`id`),
+    PRIMARY KEY(`id`),
+    CONSTRAINT FOREIGN KEY(`preview_variant_id`) REFERENCES `variants` (`id`)
+);
+ALTER TABLE `variants` ADD CONSTRAINT FOREIGN KEY (`item_id`) REFERENCES `items` (`id`);
+
+CREATE TABLE `rows` (
+    `id` int NOT NULL AUTO_INCREMENT,
+    `is_complete` boolean DEFAULT false,
+    `is_deleted` boolean DEFAULT false,
+    `sheet_id` int NOT NULL,
+    `item_id` int NOT NULL,
+    PRIMARY KEY(`id`),
+    CONSTRAINT FOREIGN KEY (`sheet_id`) REFERENCES `sheets` (`id`),
+    CONSTRAINT FOREIGN KEY (`item_id`) REFERENCES `items` (`id`)
+);
+
+CREATE TABLE `row_item_variants` (
+    `is_owned` boolean DEFAULT false,
+    `row_id` int NOT NULL,
+    `variant_id` int NOT NULL,
+    CONSTRAINT FOREIGN KEY (`row_id`) REFERENCES `rows` (`id`),
+    CONSTRAINT FOREIGN KEY (`variant_id`) REFERENCES `variants` (`id`)
+);
+
+CREATE TABLE `tags` (
+    `id` int NOT NULL AUTO_INCREMENT,
+    `category` varchar(40) NOT NULL,
+    `content` varchar(80) NOT NULL,
+    PRIMARY KEY(`id`)
+);
+
+CREATE TABLE `item_tags` (
+    `tag_id` int NOT NULL,
+    `item_id` int NOT NULL,
+    CONSTRAINT FOREIGN KEY(`tag_id`) REFERENCES tags (`id`),
+    CONSTRAINT FOREIGN KEY(`item_id`) REFERENCES items (`id`)
+);
